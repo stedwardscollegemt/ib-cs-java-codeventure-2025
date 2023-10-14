531fa3c28ce72c59636776c35f8e44b13228321a
erikacamilleri <erika.camilleri@gmail.com>
2023-10-14 17:52:31 +0200

    Add chapter 2 notes and lesson exercise

--- chapters/src/chapter02/BuildingBlocks.java
+++ chapters/src/chapter02/BuildingBlocks.java
@@ -1,7 +1,54 @@
 package chapter02;
 
+/**
+ * The { } are "scope"
+ */
 public class BuildingBlocks {
+
     public static void main(String[] args) {
-        // TODO: Start following the lesson here
+
+        // 2.5
+        // TODO: Declare and assign a constant for NO_OF_WEEK_DAYS
+
+
+        // 2.2, 2.3, 2.4, 2.6
+        // The following data types are also referred to as "primitive data types"
+
+        // Declaring a variable of data type integer and assigning a value
+        double appleUnitPrice = 0.55; // price in dollars
+        // Display "Apple Unit Price: $<appleUnitPrice>"
+        System.out.println("Apple Unit Price: $" + appleUnitPrice);
+
+        // TODO: Declaring a variable of data type int for applyQty and assign 10 as a value
+
+        // TODO: Display "Qty: 10"
+        
+        // TODO: Declare a variable named amountDue and assign it to appleUnitPrice * appleQty
+        
+        // TODO: Display "Amount due: $<amountDue>"
+        
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        // Declaring a variable of data type float and assigning a value
+        float temperature = 2.4f;
+        // TODO: Increase the temperature by 5.7 degrees celcius
+        
+        // Display "Temperature: <temperature> degrees celcius"
+        System.out.println("Temperature: " + temperature + " degrees celcius");
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        // TODO: Declaring a variable of data type double and assigning a value
+        
+        // TODO: Decrease the balance by 4.95 dollars
+        
+        // TODO: Display "Account Balance: $<balance>"
+        
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        // Declaring a variable of data type char and assigning a value
+        char scrabbleTile = 'E';
+
+        // Declaring a variable of data type boolean and assigning a value
+        boolean isHungry = true;
     }
 }
