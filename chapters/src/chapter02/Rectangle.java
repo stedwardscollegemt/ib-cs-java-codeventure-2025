457a69c17e2eacc0c7214c721528c3e27ff1b4f6
erikacamilleri <erika.camilleri@gmail.com>
2023-10-19 14:56:44 +0200

    Instructions for CodeAnythingThursdays

---
+++ chapters/src/chapter02/Rectangle.java
@@ -0,0 +1,15 @@
+package chapter02;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        // TODO: Display an explanation for Rectangle program
+
+        // TODO: Create variables of type double for w, l
+
+        // TODO: Create a variable area and assign it to w * l
+        // TODO: Display the area result to the user
+
+        // TODO: Create a variable perimeter and assign it to (2 * w) + (2 * l)
+        // TODO: Display the perimeter result to the user
+    }
+}
