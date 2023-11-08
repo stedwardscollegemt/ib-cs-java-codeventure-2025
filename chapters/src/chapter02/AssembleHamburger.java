14d0123a93b985b8d14c05c29bb64baa781e363c
erikacamilleri <erika.camilleri@gmail.com>
2023-11-08 09:15:09 +0100

    Warm-up exercise after holidays

---
+++ chapters/src/chapter02/AssembleHamburger.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+/**
+ * TODO: 
+ * display in pseudocode (as comments) all the steps
+ * to assemble a hamburger
+ */
+public class AssembleHamburger {
+    
+    public static void main(String[] args) {
+        
+    }
+}
