ff2fa03179af5257b6320c673b180aa255f3e1b3
erikacamilleri <erika.camilleri@gmail.com>
2023-11-15 09:31:05 +0100

    Added comments to IsItEven

--- chapters/src/chapter03/IsItEven.java
+++ chapters/src/chapter03/IsItEven.java
@@ -14,9 +14,14 @@ public class IsItEven {
         // Make a Scanner object and store it in input
         // This helps to get a number from the user
         Scanner input = new Scanner(System.in);
+
+        // Onboarding the user
+        System.out.println("Welcome, this program checks whether your number is odd or even.");
         
         // The number typed in by the user
         // is stored as an integer
+        // Display an instruction to the user
+        System.out.println("Please enter your number: ");
         int number = input.nextInt();
         
         // Mod operator to get remainder
