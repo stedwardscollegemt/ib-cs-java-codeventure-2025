d1a44f9e69220dd3257406622f061ac7964613ee
erikacamilleri <erika.camilleri@gmail.com>
2023-11-16 14:42:32 +0100

    Challenge BMI Calculator

---
+++ chapters/src/chapter03/BMICalculator.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+
+        // TODO: Display a title for your BMI Calculator
+
+        // TODO: Ask the user to input their personal details (height in m, weight in kg)
+
+        // TODO: Use the input values to calculate the BMI
+        // bmi = weight / (m * m)
+
+        // TODO: if bmi < 18.5 then "Underweight" etc...
+    }
+}
