f036c517bb1d45b8e0db8dfb470135775d4de91a
erikacamilleri <erika.camilleri@gmail.com>
2023-11-22 10:00:14 +0100

    Challenge NineLives

---
+++ chapters/src/chapter04/NineLives.java
@@ -0,0 +1,31 @@
+package chapter04;
+
+public class NineLives {
+    public static void main(String[] args) {
+        
+        // TODO: Display a title message with an introduction
+
+        // TODO: Declare and assign a variable to store a word of type String
+
+        // Declare and assign a variable to store lives left
+        int lives = 9;
+
+        // Declare and assign a variable to store isWordGuessed
+        boolean isWordGuessed = false;
+
+        while(lives > 0 || isWordGuessed == true) {
+            
+            // TODO: Display a clue to the user
+
+            // TODO: Guess a word or a letter
+        }
+        
+        if (lives == 0) {
+            // TODO: Tell the user they lost, and reveal the word
+        }
+
+        if (isWordGuessed) {
+            // TODO: Tell the user they won!
+        }
+    }
+}
