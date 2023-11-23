9a8728498bb6adfe42c90a2fb20e94164fbd80b3
erikacamilleri <erika.camilleri@gmail.com>
2023-11-23 15:33:32 +0100

    Easy model answer for NineLives

--- chapters/src/chapter04/NineLives.java
+++ chapters/src/chapter04/NineLives.java
@@ -1,11 +1,22 @@
 package chapter04;
 
+import helpers.Keyboard;
+
 public class NineLives {
+    
     public static void main(String[] args) {
         
-        // TODO: Display a title message with an introduction
+        // Display a title message with an introduction
+        System.out.println("Hello there, let's play: Nine Lives.");
+        System.out.println("------------------------------------");
+        System.out.println("Instructions:");
+        System.out.println("You have nine chances to guess a secret word.");
+        System.out.println("We will give you some clues, but guessing the word will not be easy.");
+        System.out.println("");
+        System.out.println("");
 
-        // TODO: Declare and assign a variable to store a word of type String
+        // Declare and assign a variable to store a word of type String
+        String word = "catfish";
 
         // Declare and assign a variable to store lives left
         int lives = 9;
@@ -13,19 +24,27 @@ public class NineLives {
         // Declare and assign a variable to store isWordGuessed
         boolean isWordGuessed = false;
 
-        while(lives > 0 || isWordGuessed == true) {
+        while(lives > 0 && isWordGuessed == false) {
             
-            // TODO: Display a clue to the user
+            // Display a clue to the user
+            System.out.println("Hint: _ _ _ _ _ _ _");
 
-            // TODO: Guess a word or a letter
+            // Guess a word or a letter
+            System.out.println("Have a guess...");
+            String guess = Keyboard.readString();
+
+            // Remove a life so that eventually the while loop will come to a stop
+            lives = lives - 1;
         }
         
         if (lives == 0) {
-            // TODO: Tell the user they lost, and reveal the word
+            // Tell the user they lost, and reveal the word
+            System.out.println("You have lost! The secret word is: " + word);
         }
 
         if (isWordGuessed) {
-            // TODO: Tell the user they won!
+            // Tell the user they won!
+            System.out.println("Wow. You managed to guess the word. Super!");
         }
     }
 }
