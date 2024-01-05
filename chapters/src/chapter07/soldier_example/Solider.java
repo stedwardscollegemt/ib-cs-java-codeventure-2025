c74841fae4ecdfe5e8542b42957acf0744bf22e2
erikacamilleri <erika.camilleri@gmail.com>
2024-01-05 10:33:38 +0100

    Creating our first class Soldier

---
+++ chapters/src/chapter07/soldier_example/Solider.java
@@ -0,0 +1,22 @@
+package chapter07.soldier_example;
+
+public class Solider {
+
+    // ----- Data ---------------------------------------
+
+    String id; // stores a unique identifier for the soldier
+
+    int health;
+
+    String currentWeapon;
+
+    String troop;
+
+    String[] profile; // name, surname, age, weakness, strength, interests
+
+    String[] weapons;
+
+
+    // ----- Actions ------------------------------------
+    
+}
