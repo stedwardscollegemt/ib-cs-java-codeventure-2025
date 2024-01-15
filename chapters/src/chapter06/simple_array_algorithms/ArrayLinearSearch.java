0b9a654c4700abc1903e856767c869247ae8dd60
erikacamilleri <erika.camilleri@gmail.com>
2024-01-15 09:35:45 +0100

    Array linear search v imp for IB

---
+++ chapters/src/chapter06/simple_array_algorithms/ArrayLinearSearch.java
@@ -0,0 +1,21 @@
+package chapter06.simple_array_algorithms;
+
+public class ArrayLinearSearch {
+
+    public static void main(String[] args) {
+
+        // created a fixed array of integers
+        int[] myNumbers = {10, 14, 19, 26, 27, 31, 33, 35, 42, 44};
+
+        // todo: Display we have a secret array of numbers, type a number, and we tell you
+        //       if you guessed
+
+        // todo: Grab a number as user input
+
+        // todo: start from the beginning
+
+        //     todo: sequentially visit each element and compare it to a target
+
+        //     todo: stop search if the target is equal to visited element
+    } 
+}
