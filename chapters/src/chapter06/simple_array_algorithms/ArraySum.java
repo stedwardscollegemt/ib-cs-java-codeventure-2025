545d23ffb64ad6452606f5a6485aeac9263cbe2e
erikacamilleri <erika.camilleri@gmail.com>
2024-01-17 09:15:28 +0100

    Practice while loop and for loop in ArraySum.java

--- chapters/src/chapter06/simple_array_algorithms/ArraySum.java
+++ chapters/src/chapter06/simple_array_algorithms/ArraySum.java
@@ -1,18 +1,38 @@
 package chapter06.simple_array_algorithms;
 
+import java.util.Scanner;
+
 public class ArraySum {
 
     public static void main(String[] args) {
 
-        // todo: display title + introduction
+        // display title + introduction
+        System.out.println("******* A Simple Array Sum Program *******");
+        System.out.println("------------------------------------------");
+        System.out.println("Give us 10 numbers and we calc the sum.");
+        System.out.println("");
         
-        // todo: declare an array of integer values called numbers
+        // declare an array of integer values called numbers
+        int[] numbers = new int[10];
 
-        // todo: ask the user to give you numbers and put them in the array
+        // ask the user to give you numbers and put them in the array
+        Scanner sc = new Scanner(System.in);
+        int index = 0;
+        while (index < 10) {
+            System.out.println("Give us a number: ");
+            numbers[index] = sc.nextInt();
+            index = index + 1;
+        }
+        sc.close();
 
-        // todo: use a for loop to calculate the sum
+        // use a for loop to calculate the sum
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
 
-        // todo: display the sum
+        // display the sum
+        System.out.println("The sum is: " + sum);
     }
     
 }
