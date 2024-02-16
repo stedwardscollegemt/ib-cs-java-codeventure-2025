5883678a425edac54d9fea9ace2f3aff1a9f8ae9
erikacamilleri <erika.camilleri@gmail.com>
2024-02-16 15:01:52 +0100

    2D Arrays Checkers Example

--- chapters/src/chapter06/hlextension/Checkers.java
+++ chapters/src/chapter06/hlextension/Checkers.java
@@ -8,18 +8,40 @@ public class Checkers {
     static char WHITE = 'W';
     
     public static void main(String[] args) {
-
+        init();
+        displayBoard();
     }
 
     public static void init() {
         // placing black play pieces
         for (int row = 0; row < 3; row++) {
-
+            for(int col = 0; col < board[0].length; col++) {
+                if (row % 2 == 0 && col % 2 == 1) {
+                    board[row][col] = BLACK;
+                } else if (row % 2 == 1 && col % 2 == 0) {
+                    board[row][col] = BLACK;
+                }
+            }
         }
 
         // placing white play pieces
         for (int row = 5; row < 8; row++) {
+            for(int col = 0; col < board[0].length; col++) {
+                if (row % 2 == 0 && col % 2 == 1) {
+                    board[row][col] = WHITE;
+                } else if (row % 2 == 1 && col % 2 == 0) {
+                    board[row][col] = WHITE;
+                }
+            }
+        }
+    }
 
+    public static void displayBoard() {
+        for (int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[0].length; col++) {
+                System.out.print("| " + board[row][col] + "| ");
+            }
+            System.out.println("");
         }
     }
 }
