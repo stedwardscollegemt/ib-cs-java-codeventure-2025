571208a91f45f2c6d940af38c46f3a61ff95536a
erikacamilleri <erika.camilleri@gmail.com>
2024-02-16 14:47:14 +0100

    Added Checkers to practice 2D arrays

---
+++ chapters/src/chapter06/hlextension/Checkers.java
@@ -0,0 +1,25 @@
+package chapter06.hlextension;
+
+public class Checkers {
+
+    // create a global two-dimensional array for my grid
+    static char[][] board = new char[8][8];
+    static char BLACK = 'B';
+    static char WHITE = 'W';
+    
+    public static void main(String[] args) {
+
+    }
+
+    public static void init() {
+        // placing black play pieces
+        for (int row = 0; row < 3; row++) {
+
+        }
+
+        // placing white play pieces
+        for (int row = 5; row < 8; row++) {
+
+        }
+    }
+}
