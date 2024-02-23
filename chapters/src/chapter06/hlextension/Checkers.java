5221d65a6828eacd425e52c18880db861d826670
erikacamilleri <erika.camilleri@gmail.com>
2024-02-23 14:48:54 +0100

    Trying to build Checkers not going so well

--- chapters/src/chapter06/hlextension/Checkers.java
+++ chapters/src/chapter06/hlextension/Checkers.java
@@ -10,6 +10,12 @@ public class Checkers {
     public static void main(String[] args) {
         init();
         displayBoard();
+
+        // testing that we can move W at board[5,0]
+        System.out.println("---------------------------------------------------");
+        moveWhite(5, 0, true);
+        System.out.println("Before: " + board[5][0] + "  After: " + board[4][1]);
+        displayBoard();
     }
 
     public static void init() {
@@ -39,9 +45,42 @@ public class Checkers {
     public static void displayBoard() {
         for (int row = 0; row < board.length; row++) {
             for(int col = 0; col < board[0].length; col++) {
-                System.out.print("| " + board[row][col] + "| ");
+                System.out.print("| " + board[row][col] + " | ");
             }
             System.out.println("");
         }
+    }
+
+    public static void moveWhite(int currentRow, int currentCol, boolean rightFlag) {
+        // todo: some validation
+        int targetRow = currentRow - 1;
+        int targetCol = 0;
+
+        if (rightFlag) {
+            targetCol = currentCol + 1;
+        } else {
+            targetCol = currentCol - 1;
+        }
+
+        if (isWhiteMoveValid(targetRow, targetCol)) {
+            // proceed with the game
+            board[targetRow][targetCol] = WHITE;
+            board[currentRow][currentCol] = ' ';
+        }
+    }
+
+    public static void attackWhite() {
+
+    }
+
+    public static boolean isWhiteMoveValid(int targetRow, int targetCol) {
+        // make sure indices are not out of bounds
+        boolean isWithinBounds = (targetRow < 8 && targetRow >= 0) && (targetCol < 8 && targetCol >= 0);
+        boolean isEmpty = board[targetRow][targetCol] != WHITE && board[targetRow][targetCol] != BLACK;
+        return isWithinBounds && isEmpty;
+    }
+
+    public static void moveBlack(int currentRow, int currentCol, boolean rightFlag) {
+
     }
 }
