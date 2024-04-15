c021fa8697cc9c72e14ba319c3efee647452f0dd
erikacamilleri <erika.camilleri@gmail.com>
2024-04-15 09:27:18 +0200

    WIP Converting UML to Java

---
+++ chapters/src/chapter08/blackjack/BlackjackRulesEngine.java
@@ -0,0 +1,27 @@
+package chapter08.blackjack;
+
+public class BlackjackRulesEngine {
+    /**
+     * -------- Data (Attributes) --------------------------
+     */
+    int numberOfPlayers;
+
+    boolean isBeingPlayed;
+
+    Player[] players;
+
+    /**
+     * -------- Constructor --------------------------
+     */
+
+     /**
+     * -------- Methods (Behaviour) --------------------------
+     */
+    public boolean isBust(int playerId) {
+        return false;
+    }
+
+    public void payout(int playerId) {
+        
+    }
+}
