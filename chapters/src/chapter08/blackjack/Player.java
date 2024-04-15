cd871cf8907c43329606d4b4975ffb85d8ccf4c5
erikacamilleri <erika.camilleri@gmail.com>
2024-04-15 09:18:24 +0200

    WIP Blackjack Convert UML to Java

---
+++ chapters/src/chapter08/blackjack/Player.java
@@ -0,0 +1,25 @@
+package chapter08.blackjack;
+
+public class Player {
+    /**
+     * -------- Data (Attributes) --------------------------
+     */
+    int id;
+
+    Chip[] chips;
+
+    /**
+     * -------- Constructor --------------------------
+     */
+
+     /**
+     * -------- Methods (Behaviour) --------------------------
+     */
+    public void tap() {}
+
+    public void hold() {}
+
+    public boolean hasChips() {
+        return false;
+    }
+}
