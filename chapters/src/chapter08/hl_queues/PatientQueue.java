2e6a3c689f0d583b3a750f034648a777d630827b
erikacamilleri <erika.camilleri@gmail.com>
2024-05-24 14:25:14 +0200

    Implemented enqueue, dequeue, isFull, isEmpty

--- chapters/src/chapter08/hl_queues/PatientQueue.java
+++ chapters/src/chapter08/hl_queues/PatientQueue.java
@@ -13,4 +13,33 @@ public class PatientQueue {
 
 
     // --- Methods ----------------------------------------------
+    public void enqueue(Patient p) {
+        if (!isFull()) {
+            tail = tail + 1;
+            queue[tail] = p;
+        }
+    }
+
+    public Patient dequeue() {
+        if (!isEmpty()) {
+            Patient p = queue[HEAD];
+            queue[HEAD] = null;
+            for (int i = 0; i < tail; i++) {
+                queue[i] = queue[i + 1];
+            }
+            queue[tail] = null;
+            tail = tail - 1;
+            return p;
+        }
+        return null;
+    }
+
+    public boolean isFull() {
+        return tail == 9;
+    }
+
+    public boolean isEmpty() {
+        return tail == -1;
+    }
+
 }
