c64672f704d7f917dc81678879a42e95479d657d
erikacamilleri <erika.camilleri@gmail.com>
2024-05-24 13:51:31 +0200

    Implementing queues using arrays

---
+++ chapters/src/chapter08/hl_queues/PatientQueue.java
@@ -0,0 +1,16 @@
+package chapter08.hl_queues;
+
+public class PatientQueue {
+    
+    // --- Data -------------------------------------------------
+    Patient[] queue = new Patient[10];
+
+    final int HEAD = 0;
+
+    int tail = -1;
+
+    // --- Constructor ------------------------------------------
+
+
+    // --- Methods ----------------------------------------------
+}
---
+++ chapters/src/chapter08/hl_queues/WaitingRoom.java
@@ -0,0 +1,5 @@
+package chapter08.hl_queues;
+
+public class WaitingRoom {
+    
+}
