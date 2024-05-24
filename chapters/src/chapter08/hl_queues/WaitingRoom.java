40a9c448d85213ba45e7af2b17ac143a6037e782
erikacamilleri <erika.camilleri@gmail.com>
2024-05-24 14:48:05 +0200

    We are trying to implement concurrency

--- chapters/src/chapter08/hl_queues/WaitingRoom.java
+++ chapters/src/chapter08/hl_queues/WaitingRoom.java
@@ -1,5 +1,18 @@
 package chapter08.hl_queues;
 
 public class WaitingRoom {
+
+    static PatientQueue patientQueue = new PatientQueue();
     
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // code goes here.
+            }
+        });  
+        t1.start();
+        
+    }
 }
